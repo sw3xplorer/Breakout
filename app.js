a484cbdb9a581fb463337bc45acc434f3d190745
Robert Popa <robert.popa@elev.ga.ntig.se>
2022-12-14 21:12:33 +0100

    stuff won't work

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 
+
 // skapa paddeln
 
 const paddleHeight = 15;
@@ -11,10 +12,17 @@ const paddleWidht = 80;
 let paddleX = (canvas.width - paddleWidht) /2;
 let paddleY = canvas.height*0.9;
 
+// paddel logik
+
+let keyDownCheck;
+let keyUpCheck;
+
+let isRightPressed = false;
+let isLeftPressed = false;
+
+
 // skapa bollen
 
-const ballHeight = 5;
-const ballWidht = ballHeight;
 let ballX = paddleX + (paddleWidht/2);
 let ballY = paddleY - 50; 
 
@@ -32,26 +40,26 @@ function collision() {
     
 }
 
+let playing = true;
 function play() {
-    let playing = true;
     if (playing)
     {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
         drawPaddle();
-        ballX += velocityX;
-        ballY += velocityY;
+        update();
         setTimeout(play, 1000 / framerate);
     }
-    
 
 }
+
 
 
 
 function drawPaddle() {
     ctx.fillStyle = "green";
     ctx.fillRect(paddleX, paddleY, paddleWidht, paddleHeight);
+
 }
 
 function drawBall() {
@@ -59,4 +67,10 @@ function drawBall() {
     ctx.beginPath()
     ctx.arc(ballX, ballY, radius, 0, 2 * Math.PI);
     ctx.fill();
+}
+
+function update()
+{
+    ballX += velocityX;
+    ballY += velocityY;
 }
